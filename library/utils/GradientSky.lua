3c259d1a09584e4fe1580579befd21806b13813d
Index <xv.media.address@gmail.com>
2024-09-21 13:36:17 -0500

    feat: document GradientSky class

---
+++ library/utils/GradientSky.lua
@@ -0,0 +1,11 @@
+---@meta
+
+---@class GradientSky
+--- GradientSky is a class that is used to set a gradient skybox in the world.
+---@field HorizonLineColor number Determines the horizon line's color.
+---@field HorizonLineExponent number Determines the horizon line's exponent.
+---@field HorizonLineContribution number Determines how much the horizon line contributes.
+---@field SkyGradientTop number Determines the top color of the gradient.
+---@field SkyGradientBottom number Determines the bottom color of the gradient.
+---@field SkyGradientExponent number Determines the gradient's exponent.
+---@field SkyDiscColor Color Determines the color emitting off the sun.
