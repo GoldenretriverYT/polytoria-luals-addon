a69727df33bcdf4d98868bbf874f9bac30ae9e3a
Index <xv.media.address@gmail.com>
2024-09-21 13:22:25 -0500

    feat: document missing enums

---
+++ library/enums/ParticleShape.lua
@@ -0,0 +1,12 @@
+---@meta
+
+---@enum ParticleShape
+ParticleShape = {
+    Sphere = 0,
+    Hemisphere = 1,
+    Cone = 2,
+    Box = 3,
+    Donut = 4,
+    Circle = 5,
+    Rectangle = 6
+}
---
+++ library/enums/ParticleSimulationSpace.lua
@@ -0,0 +1,7 @@
+---@meta
+
+---@enum ParticleSimulationSpace
+ParticleSimulationSpace = {
+	Local = 0,
+	World = 1
+}
