2bee2e4cca4ab3cabfe8e94bb4b81e56385783a1
Index <xv.media.address@gmail.com>
2024-09-21 12:47:46 -0500

    feat: document Input methods and properties

--- library/static/Input.lua
+++ library/static/Input.lua
@@ -13,6 +13,11 @@ function KeyEvent:Connect(callback) end
 ---@field MousePosition Vector2 The current position of the mouse.
 ---@field CursorLocked boolean Whether the cursor is locked or not.
 ---@field CursorVisible boolean Whether the cursor is visible or not.
+---@field AnyKey boolean Returns true if any key is being pressed.
+---@field AnyKeyDown boolean Returns true if any new key presses happened during the current frame. Held down key presses from previous frames do not count.
+---@field IsInputFocused boolean Determines whether or not the local player is currently focused on an input.
+---@field ScreenWidth integer Returns the current screen's width.
+---@field ScreenHeight integer Returns the current screen's height.
 Input = {}
 
 ---Fires when a key is pressed.
@@ -46,3 +51,57 @@ function Input:WorldToScreenPoint(position) end
 ---@param position Vector3__
 ---@return Vector3__
 function Input:WorldToViewportPoint(position) end
+
+---@param axisName string
+---@return integer
+function Input:GetAxis(axisName) end
+
+---@param axisName string
+---@return integer
+function Input:GetAxisRaw(axisName) end
+
+---@param buttonName string
+---@return boolean
+function Input:GetButton(buttonName) end
+
+---@param buttonName string
+---@return boolean
+function Input:GetButtonDown(buttonName) end
+
+---@param buttonName string
+---@return boolean
+function Input:GetButtonUp(buttonName) end
+
+---@param keyName string
+---@return boolean
+function Input:GetKey(keyName) end
+
+---@param keyName string
+---@return boolean
+function Input:GetKeyDown(keyName) end
+
+---@param keyName string
+---@return boolean
+function Input:GetKeyUp(keyName) end
+
+---@param mouseButton integer
+---@return boolean
+function Input:GetMouseButton(mouseButton) end
+
+---@param mouseButton integer
+---@return boolean
+function Input:GetMouseButtonDown(mouseButton) end
+
+---@param mouseButton integer
+---@return boolean
+function Input:GetMouseButtonUp(mouseButton) end
+
+---@param position Vector3
+---@param ignoreList? table
+---@return RayResult
+function Input:ScreenPointToRay(position, ignoreList) end
+
+---@param position Vector3
+---@param ignoreList? table
+---@return RayResult
+function Input:ViewportPointToRay(position, ignoreList) end
