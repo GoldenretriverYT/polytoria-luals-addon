cdbdf90cb2bd2d3b96ca2fd0811c4299f64c8a40
Index <xv.media.address@gmail.com>
2024-09-21 14:14:48 -0500

    minor: document Color property of Decal class

--- library/utils/Decal.lua
+++ library/utils/Decal.lua
@@ -2,6 +2,7 @@
 
 ---@class Decal: DynamicInstance
 --- Decals are objects that can have an image texture and are placed in the world.
+---@field Color Color Determines the color of the decal.
 ---@field ImageType ImageType The type of image that the decal is using.
 ---@field ImageID number The ID of the image that the decal is using.
 ---@field TextureOffset Vector2 The offset of the texture.
